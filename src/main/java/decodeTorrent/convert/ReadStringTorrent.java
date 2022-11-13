00823d78f1871101a8cf2c84950ee665edbc0db4
Vova002 <mr.vproen7shchsgjg@gmail.com>
2022-11-13 17:31:16 +0200

    nothing new

--- src/main/java/decodeTorrent/convert/ReadStringTorrent.java
+++ src/main/java/decodeTorrent/convert/ReadStringTorrent.java
@@ -2,23 +2,53 @@ package decodeTorrent.convert;
 
 import decodeTorrent.convert.data.Torrent;
 
+import java.util.ArrayList;
+
 public class ReadStringTorrent {
 
-    private String torrent;
+    private final ArrayList<String> torrentMass = new ArrayList<>();
+    private final Torrent info = new Torrent();
 
 
     public ReadStringTorrent(String _torrent){
-        torrent = _torrent;
+        cutString(_torrent);
     }
 
 
+    //Дозволяє обрізати string на кілька строк і помістити це в масив.
+    private void cutString(String torrent){
+        String[] split = torrent.split("\n");
+
+        for(String element : split){ // delete all empty str.
+            if(!element.isEmpty()){
+                torrentMass.add(element);
+            }
+        }
+    }
+
+
+
     public Torrent stringToTorrent(){
+        readElements();
         return new Torrent();
     }
 
 
     private void readElements(){
+        for(String element : torrentMass){
+
+            if (element.contains("info")){
+                readInfo();
+                break;
+            }
+        }
+    }
+
+
+    private void readInfo(){
 
     }
+
+
 
 }
